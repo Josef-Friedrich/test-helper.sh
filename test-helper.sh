f2c1abe67daaacab7c0ae58736caf1cdf77196a7
Josef Friedrich <josef@friedrich.rocks>
2017-08-12 16:02:30 +0200

    Add first real tests

--- test-helper.sh
+++ test-helper.sh
@@ -1,12 +1,24 @@
 #! /bin/sh
 
-_set_mocking_path() {
-  PATH="$(pwd)/../bin/$1:$PATH"
+mock_path() {
+	SAVEIFS=$IFS
+	IFS=:
+	local TMP_PATH
+	TMP_PATH=
+	if [ -n "$PARENT_MOCK_PATH" ]; then
+		for P in $1 ; do
+			TMP_PATH="${TMP_PATH}${PARENT_MOCK_PATH}/${P}:"
+		done
+		export PATH="${TMP_PATH}${PATH}"
+	else
+		export PATH="$1:${PATH}"
+	fi
+	IFS=$SAVEIFS
 }
 
-_source_until_separator() {
-  local TMP_FILE=$(mktemp)
-  local SEPARATOR='### This SEPARATOR is needed for the tests. Do not remove it! ##########'
-  sed "/$SEPARATOR/Q" "$1" > "$TMP_FILE"
-  . "$TMP_FILE"
+source_script() {
+	local TMP_FILE=$(mktemp)
+	local SEPARATOR='### This SEPARATOR is needed for the tests. Do not remove it! ##########'
+	sed "/$SEPARATOR/Q" "$1" > "$TMP_FILE"
+	. "$TMP_FILE"
 }
